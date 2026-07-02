f9f8b0341fb25b91f8d8d6b8fdf2c0593ab59337
Jelita-s <jelitasianturiiii@gmail.com>
2026-07-02 15:14:08 +0700

    menambahkan placeholder untuk memasukan gambar

--- js/data.js
+++ js/data.js
@@ -9,7 +9,8 @@ const PRODUCTS = [
     category: "moisturizer",
     price: 121000,
     desc: "Mengandung antioksidan, ceramide, dan hyaluronic acid yang membantu mengatasi tanda penuaan dini, noda hitam, kulit kering, dan kusam.",
-    link: "https://shopee.co.id/-Manohara's-Pick-Finally-Found-You!-SOY-BRIGHT!-Moisturizer-2in1-Primer-Makeup-dan-Gel-Pelembab-Wajah-Siang-dan-Malam-untuk-Semua-Jenis-Kulit-i.856580029.18444376450"
+    link: "https://shopee.co.id/-Manohara's-Pick-Finally-Found-You!-SOY-BRIGHT!-Moisturizer-2in1-Primer-Makeup-dan-Gel-Pelembab-Wajah-Siang-dan-Malam-untuk-Semua-Jenis-Kulit-i.856580029.18444376450",
+    image: "#"
   },
   {
     id: 2,
@@ -19,7 +20,8 @@ const PRODUCTS = [
     category: "toner",
     price: 141000,
     desc: "Mengandung AHA (Glycolic Acid), antioksidan, ceramide, mineral, dan panthenol untuk mengatasi jerawat, pori-pori besar, dan kemerahan.",
-    link: "https://shopee.co.id/-Manohara's-Pick-Finally-Found-You!-HYDRICEING-BRIGHTENING-Essence-Booster-Serum-Infused-Toner-Untuk-Semua-Jenis-Kulit-100mL-i.856580029.24064685065"
+    link: "https://shopee.co.id/-Manohara's-Pick-Finally-Found-You!-HYDRICEING-BRIGHTENING-Essence-Booster-Serum-Infused-Toner-Untuk-Semua-Jenis-Kulit-100mL-i.856580029.24064685065",
+    image: "#"
   },
   {
     id: 3,
@@ -29,7 +31,8 @@ const PRODUCTS = [
     category: "sunscreen",
     price: 110900,
     desc: "Mengandung ceramide dan panthenol untuk mengatasi jerawat, menjaga kelembapan, dan memperkuat skin barrier.",
-    link: "https://shopee.co.id/-Manohara's-Pick-Finally-Found-You!-SUN-RICE-ALL-DAY-Invisible-UV-Shield-SPF-50-PA-50ml-Sunscreen-Wajah-i.856580029.27327898085"
+    link: "https://shopee.co.id/-Manohara's-Pick-Finally-Found-You!-SUN-RICE-ALL-DAY-Invisible-UV-Shield-SPF-50-PA-50ml-Sunscreen-Wajah-i.856580029.27327898085",
+    image: "#"
   },
   {
     id: 4,
@@ -39,7 +42,8 @@ const PRODUCTS = [
     category: "cleanser",
     price: 101899,
     desc: "Mengandung cucumber, olive leaf extract, rosewater, soybean, dan sunflower oil. 2in1 Makeup Remover dan Sabun Cuci Muka pH Balanced.",
-    link: "https://shopee.co.id/-Manohara's-Pick-Finally-Found-You!-SOY-GENTLE!-Face-Cleanser-i.856580029.14492559708"
+    link: "https://shopee.co.id/-Manohara's-Pick-Finally-Found-You!-SOY-GENTLE!-Face-Cleanser-i.856580029.14492559708",
+    image: "#"
   },
   {
     id: 5,
@@ -49,7 +53,8 @@ const PRODUCTS = [
     category: "serum",
     price: 136000,
     desc: "Mengandung ceramide, kojic acid, micellar cysteamine, dan soybean extract untuk memudarkan noda hitam dan hiperpigmentasi.",
-    link: "https://shopee.co.id/-Manohara's-Pick-Finally-Found-You!-SOY-CLEAR-Brightening-Dark-Spot-Serum-i.856580029.26551470860"
+    link: "https://shopee.co.id/-Manohara's-Pick-Finally-Found-You!-SOY-CLEAR-Brightening-Dark-Spot-Serum-i.856580029.26551470860",
+    image: "#"
   },
   {
     id: 6,
@@ -59,7 +64,8 @@ const PRODUCTS = [
     category: "serum",
     price: 131000,
     desc: "Mengandung salicylic acid yang membantu mencerahkan kulit serta menjaga kebersihan pori-pori.",
-    link: "https://shopee.co.id/-Manohara's-Pick-Finally-Found-You!-RICE-BRIGHT-GLOW-SERUM-20ml-i.856580029.41102715617"
+    link: "https://shopee.co.id/-Manohara's-Pick-Finally-Found-You!-RICE-BRIGHT-GLOW-SERUM-20ml-i.856580029.41102715617",
+    image: "#"
   },
   {
     id: 7,
@@ -69,7 +75,8 @@ const PRODUCTS = [
     category: "moisturizer",
     price: 136000,
     desc: "Mengandung retinal complex, colloidal oatmeal, oat, rice extract, dan soybean untuk menyamarkan tanda penuaan dini.",
-    link: "https://shopee.co.id/Finally-Found-You!-FFY-OATstanding-Retinal-2in1-Eye-Face-Cream-i.856580029.23487862662"
+    link: "https://shopee.co.id/Finally-Found-You!-FFY-OATstanding-Retinal-2in1-Eye-Face-Cream-i.856580029.23487862662",
+    image: "#"
   },
   {
     id: 8,
@@ -79,7 +86,8 @@ const PRODUCTS = [
     category: "sunscreen",
     price: 126000,
     desc: "Mengandung panthenol, allantoin, dan green tea leaf extract untuk kulit berjerawat dan sensitif.",
-    link: "https://shopee.co.id/Finally-Found-You!-Acne-ProtecTea-Physical-Sunscreen-SPF-30-PA-50g-i.856580029.51401692620"
+    link: "https://shopee.co.id/Finally-Found-You!-Acne-ProtecTea-Physical-Sunscreen-SPF-30-PA-50g-i.856580029.51401692620",
+    image: "#"
   },
   {
     id: 9,
@@ -89,7 +97,8 @@ const PRODUCTS = [
     category: "mask",
     price: 92000,
     desc: "Mengandung niacinamide, hyaluronic acid, hydrolyzed collagen, dan rice bran water untuk menjaga kelembapan dan mengatasi kulit kusam.",
-    link: "https://shopee.co.id/Finally-Found-You!-RICE-UP!-Bright-Smooth-PEEL-OFF-MASK-50gr-i.856580029.29868385517"
+    link: "https://shopee.co.id/Finally-Found-You!-RICE-UP!-Bright-Smooth-PEEL-OFF-MASK-50gr-i.856580029.29868385517",
+    image: "#"
   },
   {
     id: 10,
@@ -99,7 +108,8 @@ const PRODUCTS = [
     category: "moisturizer",
     price: 131000,
     desc: "Mengandung AHA, BHA, peach, dan snail mucin untuk mengatasi jerawat, penuaan dini, komedo, dan noda hitam.",
-    link: "https://shopee.co.id/Finally-Found-You!-TRIPLE-VITAMIN-B-ARRIER-PEACH-SNAIL-MUCIN-Intensive-Soothing-Moisturizing-Cream-50gr-i.856580029.29985600143"
+    link: "https://shopee.co.id/Finally-Found-You!-TRIPLE-VITAMIN-B-ARRIER-PEACH-SNAIL-MUCIN-Intensive-Soothing-Moisturizing-Cream-50gr-i.856580029.29985600143",
+    image: "#"
   },
   {
     id: 11,
@@ -109,7 +119,8 @@ const PRODUCTS = [
     category: "serum",
     price: 121000,
     desc: "Mengandung salicylic acid, LHA, niacinamide, zinc PCA, dan oat extract untuk merawat kulit berjerawat.",
-    link: "https://shopee.co.id/Finally-Found-You!-OATSTANDING-BHA-LHA-Acne-Serum-i.856580029.28411975834"
+    link: "https://shopee.co.id/Finally-Found-You!-OATSTANDING-BHA-LHA-Acne-Serum-i.856580029.28411975834",
+    image: "#"
   },
   {
     id: 12,
@@ -119,7 +130,8 @@ const PRODUCTS = [
     category: "serum",
     price: 111000,
     desc: "Mengandung AHA, BHA, PHA, LHA untuk menghaluskan, mencerahkan, dan mengangkat sel kulit mati.",
-    link: "https://shopee.co.id/Finally-Found-You!-BERRY-DEEP-Exfoliating-Serum-20ml-i.856580029.27654540602"
+    link: "https://shopee.co.id/Finally-Found-You!-BERRY-DEEP-Exfoliating-Serum-20ml-i.856580029.27654540602",
+    image: "#"
   },
   {
     id: 13,
@@ -129,7 +141,8 @@ const PRODUCTS = [
     category: "cleanser",
     price: 76000,
     desc: "Mengandung deep sea water, guaiazulene, allantoin, dan blue pea extract untuk menenangkan dan memperkuat skin barrier.",
-    link: "https://shopee.co.id/Finally-Found-You!-Ocean-Blue-Pea-Dual-Phase-Micellar-Water-100ml-i.856580029.42256253477"
+    link: "https://shopee.co.id/Finally-Found-You!-Ocean-Blue-Pea-Dual-Phase-Micellar-Water-100ml-i.856580029.42256253477",
+    image: "#"
   },
   {
     id: 14,
@@ -139,7 +152,8 @@ const PRODUCTS = [
     category: "toner",
     price: 131000,
     desc: "Mengandung AHA, BHA, antioksidan, mineral, dan bahan organik untuk mengatasi jerawat, penuaan dini, dan komedo.",
-    link: "https://shopee.co.id/Finally-Found-You!-Calm-Dula-HOCL-Essence-Mist-100mL-i.856580029.42716839637"
+    link: "https://shopee.co.id/Finally-Found-You!-Calm-Dula-HOCL-Essence-Mist-100mL-i.856580029.42716839637",
+    image: "#"
   },
   {
     id: 15,
@@ -149,7 +163,8 @@ const PRODUCTS = [
     category: "toner",
     price: 126000,
     desc: "Mengandung panthenol, vitamin B12, allantoin, snail extract, peach extract, dan niacinamide untuk memperkuat skin barrier.",
-    link: "https://shopee.co.id/Finally-Found-You!-Triple-Vitamin-B-Arrier-Peach-Snail-Mucin-Intensive-Soothing-Essence-Toner-100mL-i.856580029.43211174429"
+    link: "https://shopee.co.id/Finally-Found-You!-Triple-Vitamin-B-Arrier-Peach-Snail-Mucin-Intensive-Soothing-Essence-Toner-100mL-i.856580029.43211174429",
+    image: "#"
   },
   {
     id: 16,
@@ -159,7 +174,8 @@ const PRODUCTS = [
     category: "serum",
     price: 126000,
     desc: "Mengandung AHA, BHA, hyaluronic acid, peptide, dan vitamin C untuk mengatasi jerawat dan noda secara tepat sasaran.",
-    link: "https://shopee.co.id/Finally-Found-You!-Acne-ProtecTea-Spot-Treatment-12g-i.856580029.51301692566"
+    link: "https://shopee.co.id/Finally-Found-You!-Acne-ProtecTea-Spot-Treatment-12g-i.856580029.51301692566",
+    image: "#"
   },
   {
     id: 17,
@@ -169,7 +185,8 @@ const PRODUCTS = [
     category: "body",
     price: 51000,
     desc: "Mengandung Vitamin C & Retinol untuk mencerahkan, mengencangkan, dan melembapkan kulit serta menyamarkan noda.",
-    link: "https://shopee.co.id/-Amanda-Zahra's-Pick-Finally-Found-You!-BLACK-RICE-AI-High-Concentrated-Brightening-Body-Soap-i.856580029.50555404293"
+    link: "https://shopee.co.id/-Amanda-Zahra's-Pick-Finally-Found-You!-BLACK-RICE-AI-High-Concentrated-Brightening-Body-Soap-i.856580029.50555404293",
+    image: "#"
   },
   {
     id: 18,
@@ -179,7 +196,8 @@ const PRODUCTS = [
     category: "body",
     price: 126000,
     desc: "Mengandung Glycolic Acid, Kojic Acid & Alpha Arbutin, Hyaluronic Acid dan Ceramide untuk kulit lebih halus dan cerah.",
-    link: "https://shopee.co.id/-Amanda-Zahra's-Pick-Finally-Found-You!-BLACK-RICE-AI-High-Concentrated-Brightening-Body-Scrub-i.856580029.54057538203"
+    link: "https://shopee.co.id/-Amanda-Zahra's-Pick-Finally-Found-You!-BLACK-RICE-AI-High-Concentrated-Brightening-Body-Scrub-i.856580029.54057538203",
+    image: "#"
   },
   {
     id: 19,
@@ -189,7 +207,8 @@ const PRODUCTS = [
     category: "body",
     price: 136000,
     desc: "Mengandung Retinal, Niacinamide, Kojic Acid, Alpha Arbutin, Glutathione, dan Rice Extract untuk kulit lebih cerah dan sehat.",
-    link: "https://shopee.co.id/Finally-Found-You!-RICE-AI-High-Concentrated-Brightening-Body-Mask-i.856580029.41279371656"
+    link: "https://shopee.co.id/Finally-Found-You!-RICE-AI-High-Concentrated-Brightening-Body-Mask-i.856580029.41279371656",
+    image: "#"
   },
   {
     id: 20,
@@ -199,7 +218,8 @@ const PRODUCTS = [
     category: "body",
     price: 186000,
     desc: "Mengandung Alpha Arbutin, Tranexamic Acid, Kojic Acid, Allantoin dan Black Rice Extract untuk mencerahkan kulit badan.",
-    link: "https://shopee.co.id/Finally-Found-You!-The-Ultimate-Concentrate-NANO-WHITE%E2%84%A2-BLACK-RICE-For-Body-Serum-Badan-Pencerah-30mL-i.856580029.40279392908"
+    link: "https://shopee.co.id/Finally-Found-You!-The-Ultimate-Concentrate-NANO-WHITE%E2%84%A2-BLACK-RICE-For-Body-Serum-Badan-Pencerah-30mL-i.856580029.40279392908",
+    image: "#"
   },
   {
     id: 21,
@@ -209,7 +229,8 @@ const PRODUCTS = [
     category: "body",
     price: 116000,
     desc: "Mengandung 15% Niacinamide, Tranexamic Acid, Alpha Arbutin, Rice Extract dan Peptide untuk kulit glowing.",
-    link: "https://shopee.co.id/-Amanda-Zahra's-Pick-Finally-Found-You!-RICE-AI-High-Concentrated-Brightening-Body-Lotion-i.856580029.54906304166"
+    link: "https://shopee.co.id/-Amanda-Zahra's-Pick-Finally-Found-You!-RICE-AI-High-Concentrated-Brightening-Body-Lotion-i.856580029.54906304166",
+    image: "#"
   },
   {
     id: 22,
@@ -219,7 +240,8 @@ const PRODUCTS = [
     category: "body",
     price: 136000,
     desc: "Mengandung Niacinamide, Peptide, Rice Bran Extract dan UV Protector untuk proteksi dan mencerahkan kulit badan.",
-    link: "https://shopee.co.id/Finally-Found-You!-RICE-AI-UV-Bright-Tone-Up-Lotion-SPF-30-PA-i.856580029.42378822793"
+    link: "https://shopee.co.id/Finally-Found-You!-RICE-AI-UV-Bright-Tone-Up-Lotion-SPF-30-PA-i.856580029.42378822793",
+    image: "#"
   },
   {
     id: 23,
@@ -229,6 +251,7 @@ const PRODUCTS = [
     category: "lip",
     price: 81000,
     desc: "Mengandung Vitamin C, Ubiquinone, dan Manuka Honey untuk mencerahkan, melembapkan, serta menyamarkan bibir gelap.",
-    link: "https://shopee.co.id/Finally-Found-You!-C-U-Honey-Lip-Serum-untuk-Melembapkan-dan-Mencerahkan-Bibir-5ml-i.856580029.26865115812"
+    link: "https://shopee.co.id/Finally-Found-You!-C-U-Honey-Lip-Serum-untuk-Melembapkan-dan-Mencerahkan-Bibir-5ml-i.856580029.26865115812",
+    image: "#"
   }
 ];
--- js/ui.js
+++ js/ui.js
@@ -55,40 +55,9 @@ function starsHTML(rating) {
   return out;
 }
 
-/* ---------- Category -> product illustration (inline SVG) ---------- */
-const CATEGORY_TINT = {
-  serum:       '#7c3aed',
-  moisturizer: '#db2777',
-  cleanser:    '#059669',
-  toner:       '#2563eb',
-  sunscreen:   '#d97706',
-  mask:        '#e11d48',
-  body:        '#16a34a',
-  lip:         '#e11d48'
-};
-
-function productIcon(category) {
-  const c = CATEGORY_TINT[category] || '#7c3aed';
-  switch (category) {
-    case 'serum':
-      return `<svg viewBox="0 0 100 130" fill="none"><path d="M38 8h24v18l10 10v78a8 8 0 0 1-8 8H36a8 8 0 0 1-8-8V36l10-10V8z" fill="${c}" opacity=".18"/><rect x="38" y="8" width="24" height="16" rx="2" fill="${c}"/><path d="M32 40h36v78a6 6 0 0 1-6 6H38a6 6 0 0 1-6-6V40z" fill="#fff" stroke="${c}" stroke-width="3"/><path d="M32 92h36v20a6 6 0 0 1-6 6H38a6 6 0 0 1-6-6V92z" fill="${c}" opacity=".85"/><circle cx="50" cy="16" r="3.2" fill="#fff"/></svg>`;
-    case 'moisturizer':
-      return `<svg viewBox="0 0 120 120" fill="none"><rect x="18" y="34" width="84" height="72" rx="14" fill="#fff" stroke="${c}" stroke-width="3.5"/><rect x="18" y="34" width="84" height="22" rx="10" fill="${c}" opacity=".85"/><rect x="30" y="14" width="60" height="24" rx="8" fill="${c}"/><circle cx="60" cy="78" r="16" fill="${c}" opacity=".16"/><circle cx="60" cy="78" r="7" fill="${c}"/></svg>`;
-    case 'cleanser':
-      return `<svg viewBox="0 0 100 140" fill="none"><rect x="24" y="46" width="52" height="82" rx="12" fill="#fff" stroke="${c}" stroke-width="3.5"/><rect x="24" y="46" width="52" height="26" fill="${c}" opacity=".85"/><rect x="38" y="18" width="24" height="30" rx="6" fill="${c}"/><path d="M62 22h14a6 6 0 0 1 6 6v6a6 6 0 0 1-6 6H62V22z" fill="${c}" opacity=".7"/><rect x="38" y="6" width="10" height="14" rx="3" fill="${c}"/></svg>`;
-    case 'toner':
-      return `<svg viewBox="0 0 100 140" fill="none"><rect x="26" y="40" width="48" height="88" rx="10" fill="#fff" stroke="${c}" stroke-width="3.5"/><rect x="26" y="40" width="48" height="20" fill="${c}" opacity=".85"/><rect x="40" y="18" width="20" height="24" rx="5" fill="${c}"/><rect x="44" y="6" width="12" height="14" rx="3" fill="${c}" opacity=".7"/></svg>`;
-    case 'sunscreen':
-      return `<svg viewBox="0 0 100 140" fill="none"><path d="M30 46h40l6 74a8 8 0 0 1-8 8H32a8 8 0 0 1-8-8l6-74z" fill="#fff" stroke="${c}" stroke-width="3.5"/><path d="M30 46h40l2 24H28l2-24z" fill="${c}" opacity=".85"/><rect x="36" y="14" width="28" height="34" rx="8" fill="${c}"/><circle cx="50" cy="100" r="14" fill="${c}" opacity=".15"/><path d="M50 90v20M40 100h20M43 93l14 14M57 93l-14 14" stroke="${c}" stroke-width="2.5" stroke-linecap="round"/></svg>`;
-    case 'mask':
-      return `<svg viewBox="0 0 120 100" fill="none"><rect x="10" y="14" width="100" height="72" rx="16" fill="#fff" stroke="${c}" stroke-width="3.5"/><path d="M10 30h100" stroke="${c}" stroke-width="3"/><circle cx="60" cy="58" r="18" fill="${c}" opacity=".18"/><path d="M52 58a8 8 0 0 1 16 0" stroke="${c}" stroke-width="3" fill="none" stroke-linecap="round"/></svg>`;
-    case 'body':
-      return `<svg viewBox="0 0 100 140" fill="none"><rect x="20" y="42" width="60" height="86" rx="14" fill="#fff" stroke="${c}" stroke-width="3.5"/><rect x="20" y="42" width="60" height="24" fill="${c}" opacity=".85"/><rect x="34" y="16" width="32" height="28" rx="7" fill="${c}"/><path d="M66 20h12a6 6 0 0 1 6 6v4a6 6 0 0 1-6 6H66V20z" fill="${c}" opacity=".7"/></svg>`;
-    case 'lip':
-      return `<svg viewBox="0 0 70 140" fill="none"><rect x="18" y="52" width="34" height="76" rx="10" fill="#fff" stroke="${c}" stroke-width="3.5"/><rect x="18" y="52" width="34" height="20" fill="${c}" opacity=".85"/><path d="M22 20c0-8 6-14 13-14s13 6 13 14v34H22V20z" fill="${c}"/></svg>`;
-    default:
-      return `<svg viewBox="0 0 100 100" fill="none"><circle cx="50" cy="50" r="34" fill="${c}" opacity=".2"/></svg>`;
-  }
+/* ---------- Product visual: real photo ---------- */
+function productVisualHTML(p) {
+  return `<img src="${p.image}" alt="${p.name}" loading="lazy">`;
 }
 
 /* ---------- Product card markup (shared by catalog + recommendation grids) ---------- */
@@ -100,7 +69,7 @@ function productCardHTML(p) {
       <div class="product-media">
         <span class="category-tag tag-${p.category}">${p.category}</span>
         <button class="wishlist-btn" onclick="toggleWishlist(${p.id}, this)" aria-label="Simpan ke wishlist">♡</button>
-        <div class="product-visual">${productIcon(p.category)}</div>
+        <div class="product-visual">${productVisualHTML(p)}</div>
       </div>
       <div class="product-info">
         <h3>${p.name}</h3>
